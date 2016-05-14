75c47a5dbd8b81a5c8f220c0ebf1d877bcde5e65
lingdian618 <lijian430421@gmail.com>
2016-05-14 09:21:28 +0800

    add scripts for installing chrome and smplayer

---
+++ scripts/install_chrome.sh
@@ -0,0 +1,6 @@
+# install google-chrome-stable on ubuntu
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+sudo apt-get update 
+sudo apt-get install -y google-chrome-stable
+
---
+++ scripts/install_smplayer.sh
@@ -0,0 +1,3 @@
+sudo add-apt-repository -y ppa:rvm/smplayer
+sudo apt-get update
+sudo apt-get install -y smplayer
